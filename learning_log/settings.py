b201ae3ccb835e629aed8874874f25dae862096d
sunyirong <995334152@qq.com>
2018-11-30 20:42:11 +0800

    完成20.1

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #第三方应用程序
+    'bootstrap3',
+
     #我的应用程序
     'learning_logs',
     'users',
@@ -125,3 +128,10 @@ STATIC_URL = '/static/'
 
 #我的设置
 LOGIN_URL = '/users/login/'
+
+# Settings for django-bootstrap3
+BOOTSTRAP3 = {
+    'include_jquery': True,
+    }
+
+
